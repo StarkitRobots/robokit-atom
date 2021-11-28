ea46127b979bc32997c6ecbc2e0c4b6eccf5e1d4
Merrah <ienjoyeatingcorpses@gmail.com>
2021-11-28 17:33:59 +0300

    motion set_servo service

--- src/core/nodes/walk_motion_node.py
+++ src/core/nodes/walk_motion_node.py
@@ -3,10 +3,11 @@
 import os
 import rospy
 import json
+import time
 from rospkg import RosPack
 
 from motion.motion_atom import Motion
-from core.srv import MotionService, WalkService
+from core.srv import MotionService, WalkService, ServoService
 
 
 class MotionServer:
@@ -21,26 +22,32 @@ class MotionServer:
         self.side_length = 0
         self.angle = 0
 
-
         self.motion_config = None
+        self.servo_config = None
 
         self.load_config()
+
         rospy.logdebug(self.motion_config)
 
     def load_config(self):
         rp = RosPack()
         core_path = rp.get_path("core")
+
         config_path = core_path + "/config/motion_config_test.json"
         with open(config_path) as f:
             self.motion_config = json.load(f)
+
+        config_path = core_path + "/config/servo_config.json"
+        with open(config_path) as f:
+            self.servo_config = json.load(f)
 
     def handle_walk(self, req):
         walk_enabled = req.walk_enabled
         self.step_length = req.step_length
         self.side_length = req.side_length
         self.angle = req.angle
-        if not self.last_step:
-            self.last_step = self.walk_enabled and not walk_enabled
+
+        self.last_step = self.walk_enabled and not walk_enabled
         self.walk_enabled = walk_enabled
         return self.walk_enabled or self.last_step
 
@@ -52,12 +59,32 @@ class MotionServer:
         self.motion.kondo.motionPlay(self.motion_config[motion_name])
         return True
 
+    def handle_servo(self, req):
+        name = req.name
+        position = req.position
+        start1 = time.perf_counter()
+        servoDatas = []
+        for i in range(len(name)):
+            pos = int(position[i]*1698 + 7500)
+            servoDatas.append(self.motion.kondo.ServoData(self.servo_config[name[i]][0],
+                                                          self.servo_config[name[i]][1],
+                                                          pos))
+        servoDatas = self.motion.reOrderServoData(servoDatas)
+            
+        a = self.motion.kondo.setServoPos(servoDatas, self.motion.frames_per_cycle)
+        
+        time1 = time.perf_counter() - start1
+        time.sleep(time1)
+        return a
+
+
     def motion_server(self):
         rospy.init_node('motion_server')
         s_num = rospy.Service('walk_service', WalkService, self.handle_walk)
         s_str = rospy.Service(
             'motion_service', MotionService, self.handle_motion)
-        rospy.loginfo(f"Launched \033[92mwalk_service\033[0m and \033[92mmotion_service\033[0m")
+        s_serv = rospy.Service('servo_srvice', ServoService, self.handle_servo)
+        rospy.loginfo(f"Launched \033[92mwalk_service\033[0m, \033[92mservo_service\033[0m and \033[92mmotion_service\033[0m")
         
         while True:
             self.motion_cycle()
@@ -70,7 +97,6 @@ class MotionServer:
 
         if self.last_step:
             print("Last step")
-            self.last_step = False
             self.walk_cycles = 0
             self.motion.walk_Cycle(
                 self.step_length, self.side_length, self.angle, 99, 100)
